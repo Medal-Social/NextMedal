ab1d8140b8195712e9db9226d7f63b7808d835c6
alioftech <ali@medalsocial.com>
2025-05-01 16:10:10 +0200

    feat: docs dir using fumadocs

---
+++ src/app/(docs)/docs/[[...slug]]/page.tsx
@@ -0,0 +1,59 @@
+import { source } from '@/lib/source';
+import {
+  DocsBody,
+  DocsDescription,
+  DocsPage,
+  DocsTitle,
+} from 'fumadocs-ui/page';
+import { notFound } from 'next/navigation';
+import { getMDXComponents } from '@/mdx-components';
+
+const Page = async (props: {
+  params: { slug?: string[] };
+}) => {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  const MDX = page.data.body;
+
+  return (
+    <DocsPage 
+      toc={page.data.toc} 
+      full={page.data.full}
+      editOnGithub={{
+        owner: 'Medal-Social',
+        repo: 'NextMedal',
+        sha: 'dev',
+        path: `content/docs/${props.params.slug?.join('/') || 'index'}.mdx`,
+      }}
+    >
+      <DocsTitle>{page.data.title}</DocsTitle>
+      {page.data.description && (
+        <DocsDescription>{page.data.description}</DocsDescription>
+      )}
+      <DocsBody>
+        <MDX components={getMDXComponents()} />
+      </DocsBody>
+    </DocsPage>
+  );
+};
+
+export async function generateStaticParams() {
+  return source.generateParams();
+}
+
+export async function generateMetadata(props: {
+  params: Promise<{ slug?: string[] }>;
+}) {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  return {
+    title: page.data.title,
+    description: page.data.description,
+  };
+}
+
+export default Page; 
