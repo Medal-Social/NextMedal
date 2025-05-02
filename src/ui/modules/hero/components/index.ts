0d0c51c134661b59160fa14978d5cb10480009c6
alioftech <ali@medalsocial.com>
2025-05-02 11:17:41 +0200

    feat(ui): add hero components barrel file for centralized exports

---
+++ src/ui/modules/hero/components/index.ts
@@ -0,0 +1,4 @@
+export { AsymmetricImageLayout } from './AsymmetricImageLayout';
+export { RegularImageLayout } from './RegularImageLayout';
+export { ImageBackground } from './ImageBackground';
+export { HeroContent } from './HeroContent';
