e935aae4394463ff9443d2eb25fd6bbaf8ed6700
alioftech <ali@medalsocial.com>
2025-05-10 21:42:46 +0200

    refactor(hero): remove Hero component and related types and layouts

--- src/ui/modules/hero/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { AsymmetricImageLayout } from './AsymmetricImageLayout';
-export { RegularImageLayout } from './RegularImageLayout';
-export { ImageBackground } from './ImageBackground';
-export { HeroContent } from './HeroContent';
