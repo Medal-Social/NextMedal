c5b65152dedc2c225176e206b0c302eb716d31e7
alioftech <ali@medalsocial.com>
2025-05-11 01:00:40 +0200

    style: reorganize imports in MDX components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,9 @@
+import { Mermaid } from '@/components/mdx/mermaid';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import { File, Files, Folder } from 'fumadocs-ui/components/files';
+import { Step, Steps } from 'fumadocs-ui/components/steps';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
-import { Mermaid } from '@/components/mdx/mermaid';
-import { Accordions, Accordion } from 'fumadocs-ui/components/accordion';
-import { File, Folder, Files } from 'fumadocs-ui/components/files';
-import { Step, Steps } from 'fumadocs-ui/components/steps';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
