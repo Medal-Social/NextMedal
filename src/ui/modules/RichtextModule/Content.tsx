507a92ee6a32fc4e3b5b07555a3a1f9555a66ec2
alioftech <ali@medalsocial.com>
2025-05-30 22:38:13 +0200

    refactor(RichtextModule): remove AnchoredHeading component and its usage in Content

--- src/ui/modules/RichtextModule/Content.tsx
+++ src/ui/modules/RichtextModule/Content.tsx
@@ -1,7 +1,6 @@
 import { cn } from '@/lib/utils';
 import { PortableText } from 'next-sanity';
 import Admonition from './Admonition';
-import AnchoredHeading from './AnchoredHeading';
 import Image from './Image';
 
 export default function Content({
@@ -14,13 +13,6 @@ export default function Content({
       <PortableText
         value={value}
         components={{
-          block: {
-            h2: (node) => <AnchoredHeading as="h2" {...node} />,
-            h3: (node) => <AnchoredHeading as="h3" {...node} />,
-            h4: (node) => <AnchoredHeading as="h4" {...node} />,
-            h5: (node) => <AnchoredHeading as="h5" {...node} />,
-            h6: (node) => <AnchoredHeading as="h6" {...node} />,
-          },
           types: {
             image: Image,
             admonition: Admonition,
