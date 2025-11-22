e88c7a68865fbf51ff97603f85bff35f21a458d3
alioftech <ali@medalsocial.com>
2025-11-22 20:19:19 +0100

    refactor: update Sanity schema fragments
    Update admonition, alignment, and schema index files.

--- src/sanity/schemaTypes/fragments/index.ts
+++ src/sanity/schemaTypes/fragments/index.ts
@@ -1,2 +1,2 @@
-export { default as imageBlock } from './image-block';
 export { default as admonition } from './admonition';
+export { default as imageBlock } from './image-block';
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -9,15 +9,32 @@ import navigation from './documents/navigation';
 import page from './documents/page';
 import redirect from './documents/redirect';
 import site from './documents/site';
-
+// objects
+import modules from './fragments/modules';
 // miscellaneous
 import announcement from './misc/announcement';
 import logo from './misc/logo';
 import person from './misc/person';
 import pricing from './misc/pricing';
-
-// objects
-import modules from './fragments/modules';
+// modules
+import accordionList from './modules/accordion-list';
+import blogFrontpage from './modules/blog-frontpage';
+import blogList from './modules/blog-list';
+import blogPostContent from './modules/blog-post-content';
+import breadcrumbs from './modules/breadcrumbs';
+import callout from './modules/callout';
+import featureGrid from './modules/feature-grid';
+import featuredHero from './modules/featured-hero';
+import galleryHero from './modules/gallery-hero';
+import hero from './modules/hero';
+import logoList from './modules/logo-list';
+import personList from './modules/person-list';
+import pricingComparison from './modules/pricing-comparison';
+import pricingList from './modules/pricing-list';
+import productComparison from './modules/productComparison';
+import richtextModule from './modules/richtext-module';
+import tabbedContent from './modules/tabbed-content';
+import videoHero from './modules/video-hero';
 import cta from './objects/cta';
 import icon from './objects/icon';
 import img from './objects/img';
@@ -26,28 +43,6 @@ import linkList from './objects/link.list';
 import metadata from './objects/metadata';
 import moduleOptions from './objects/module-options';
 import stat from './objects/stat';
-
-// modules
-import accordionList from './modules/accordion-list';
-import blogFrontpage from './modules/blog-frontpage';
-import blogList from './modules/blog-list';
-import blogPostContent from './modules/blog-post-content';
-import breadcrumbs from './modules/breadcrumbs';
-import callout from './modules/callout';
-
-import featureGrid from './modules/feature-grid';
-import featuredHero from './modules/featured-hero';
-import galleryHero from './modules/gallery-hero';
-import hero from './modules/hero';
-import logoList from './modules/logo-list';
-
-import personList from './modules/person-list';
-import pricingComparison from './modules/pricing-comparison';
-import pricingList from './modules/pricing-list';
-import productComparison from './modules/productComparison';
-import richtextModule from './modules/richtext-module';
-import tabbedContent from './modules/tabbed-content';
-import videoHero from './modules/video-hero';
 
 export const schemaTypes: SchemaTypeDefinition[] = [
   // documents
