fc48aedccb98be71f83dc728ad22f4d5c9410c11
alioftech <ali@medalsocial.com>
2025-11-22 20:20:30 +0100

    chore: update Sanity CLI configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,5 +1,5 @@
-import { dataset, projectId } from '@/sanity/lib/env';
 import { defineCliConfig } from 'sanity/cli';
+import { dataset, projectId } from '@/sanity/lib/env';
 
 export default defineCliConfig({
   api: {
