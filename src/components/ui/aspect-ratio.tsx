fe91110e2cae1517325f347d4276674044365e3f
alioftech <ali@medalsocial.com>
2025-12-20 17:31:30 +0100

    Update AspectRatio component

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,7 +1,22 @@
-'use client';
+import { cn } from '@/lib/utils';
 
-import { AspectRatio as AspectRatioPrimitive } from 'radix-ui';
-
-const AspectRatio = AspectRatioPrimitive.Root;
+function AspectRatio({
+  ratio,
+  className,
+  ...props
+}: React.ComponentProps<'div'> & { ratio: number }) {
+  return (
+    <div
+      data-slot="aspect-ratio"
+      style={
+        {
+          '--ratio': ratio,
+        } as React.CSSProperties
+      }
+      className={cn('relative aspect-(--ratio)', className)}
+      {...props}
+    />
+  );
+}
 
 export { AspectRatio };
