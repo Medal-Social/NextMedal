05a2ae5071a0a17180d2b6cd83bc0683cb5a3350
alioftech <ali@medalsocial.com>
2025-12-20 13:04:50 +0100

    feat(ui): update Img component

--- src/ui/Img.tsx
+++ src/ui/Img.tsx
@@ -66,10 +66,7 @@ export function Img({
   const loadingValue = stegaClean(sanityImage.loading);
   const validLoading = props.priority
     ? undefined
-    : _loading ||
-      (loadingValue === 'eager' || loadingValue === 'lazy'
-        ? loadingValue
-        : 'lazy');
+    : _loading || (loadingValue === 'eager' || loadingValue === 'lazy' ? loadingValue : 'lazy');
 
   if (validLoading === 'eager') {
     preload(src, { as: 'image' });
