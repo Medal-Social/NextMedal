f716dbe32bd1efe385635a338bc6c2bdd4a160f1
alioftech <ali@medalsocial.com>
2025-12-20 05:48:01 +0100

    refactor: simplify UidInput component integration
    - Replaced inline Uid input implementation with a dedicated UidInputComponent for improved code organization and maintainability.
    - Removed unnecessary state management and UI elements, streamlining the input handling process.

--- src/sanity/schemaTypes/modules/uid-input.tsx
+++ src/sanity/schemaTypes/modules/uid-input.tsx
@@ -1,9 +1,5 @@
-'use client';
-
-import { Box, Button, Flex, Text, TextInput } from '@sanity/ui';
-import { useState } from 'react';
-import { VscCheck, VscCopy } from 'react-icons/vsc';
 import { defineField } from 'sanity';
+import { UidInputComponent } from './UidInputComponent';
 
 export function createUidField() {
   return defineField({
@@ -14,31 +10,7 @@ export function createUidField() {
     validation: (Rule) =>
       Rule.regex(/^[a-zA-Z0-9-]+$/g).error('Must not contain spaces or special characters'),
     components: {
-      input: ({ elementProps, path }) => {
-        const [checked, setChecked] = useState(false);
-        const indexOfModule = path.indexOf('modules');
-        const moduleKey = (path[indexOfModule + 1] as any)?._key;
-
-        return (
-          <Flex align="center" gap={1}>
-            <Text muted>#</Text>
-            <Box flex={1}>
-              <TextInput {...elementProps} placeholder={moduleKey} />
-            </Box>
-            <Button
-              title="Click to copy"
-              mode="ghost"
-              icon={checked ? VscCheck : VscCopy}
-              disabled={checked}
-              onClick={() => {
-                navigator.clipboard.writeText(`#${elementProps.value || moduleKey}`);
-                setChecked(true);
-                setTimeout(() => setChecked(false), 1000);
-              }}
-            />
-          </Flex>
-        );
-      },
+      input: UidInputComponent,
     },
   });
 }
