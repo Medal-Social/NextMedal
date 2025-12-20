595cda8d35242fc4089806c406f8ae165f0b3dcc
alioftech <ali@medalsocial.com>
2025-12-20 05:18:47 +0100

    fix: update Banner component to fetch correct data type
    - Changed the data fetching in the Banner component to retrieve 'Sanity.Banner' instead of 'Sanity.Announcement', ensuring accurate data representation.
    - This adjustment aligns the component with the updated schema for site-wide promotions.

--- src/ui/Banner.tsx
+++ src/ui/Banner.tsx
@@ -4,7 +4,7 @@ import { LINK_QUERY } from '@/sanity/lib/queries';
 import BannerClient from './Banner-client';
 
 export default async function Banner() {
-  const banners = await fetchSanityLive<(Sanity.Announcement & Sanity.Module)[]>({
+  const banners = await fetchSanityLive<(Sanity.Banner & Sanity.Module)[]>({
     query: groq`*[_type == 'site'][0].banners[]->{
 			...,
 			cta{ ${LINK_QUERY} },
