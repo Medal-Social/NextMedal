5d10eece468f7242c3adb3087e63f7abb7578575
alioftech <ali@medalsocial.com>
2025-12-24 00:16:49 +0100

    feat(lib): update moduleProps to support overrides

--- src/lib/moduleProps.ts
+++ src/lib/moduleProps.ts
@@ -1,8 +1,15 @@
 import { stegaClean } from 'next-sanity';
 
-export default function ({ _type, options, _key }: Partial<Sanity.Module>) {
+export default function ({
+  _type,
+  options,
+  _key,
+  ...rest
+}: Partial<Sanity.Module> & { spacing?: any; width?: any }) {
   return {
     id: stegaClean(options?.uid) || `module-${_key}`,
     'data-module': _type,
+    ...(rest.spacing ? { spacing: rest.spacing } : {}),
+    ...(rest.width ? { width: rest.width } : {}),
   };
 }
