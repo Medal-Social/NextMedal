2e3e15680b6ce74a46b334b55de3c203174fea5b
alioftech <ali@medalsocial.com>
2025-12-26 18:54:12 +0200

    fix(header): update logoData type for improved type safety in BrandMenu component
    - Changed logoData type from any to Sanity.Logo to enhance type safety and clarity in the BrandMenuProps interface.

--- src/ui/header/BrandMenu.tsx
+++ src/ui/header/BrandMenu.tsx
@@ -16,7 +16,7 @@ import { urlFor } from '@/sanity/lib/image';
 
 interface BrandMenuProps {
   children: React.ReactNode;
-  logoData?: any;
+  logoData?: Sanity.Logo;
   hasBrandPage?: boolean;
 }
 
